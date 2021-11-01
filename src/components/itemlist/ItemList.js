60e8f49531bda270b87a9e07d0400f44ebd67025
Luis Dufour <luis.dfour@gmail.com>
2021-11-01 02:01:03 -0300

    responsive

--- src/components/itemlist/ItemList.js
+++ src/components/itemlist/ItemList.js
@@ -2,22 +2,30 @@ import React, {useState, useEffect} from 'react';
 import './ItemList.css';
 import datajson from '../../datosjson/datos.json';
 import Item from '../item/Item';
+import { useParams } from 'react-router-dom';
 
-const ItemList = () => {
+const ItemList = ({match}) => {
 
-    const [datos, setDatos] = useState([]);
+    const [data, setData] = useState([]);
+    const [category, setCategory] = useState(useParams().category);
 
     useEffect(()=>{ 
         setTimeout(()=>{
-            setDatos(datajson);
+            if(category == undefined){
+                setData(datajson);
+            }else{
+                setData(datajson.filter(data => data.category == category));
+            }
+            
         }, 2000)
-    }, []);
+
+    }, [category]);
 
     return (
         <>
             <div className="itemList container">
                 {
-                    datos.map((data)=>(
+                    data.map((data)=>(
                         <Item data={data} key={data.id}/>
                     )
                     )
